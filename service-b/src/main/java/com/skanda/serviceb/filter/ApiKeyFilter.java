83378421f91f8c9650d661aeafb0a9f26f10f97e
Skanda <skanda.td@gmail.com>
2025-08-28 12:52:27 +0530

    X-API-KEY Implementation

---
+++ service-b/src/main/java/com/skanda/serviceb/filter/ApiKeyFilter.java
@@ -0,0 +1,35 @@
+package com.skanda.serviceb.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class ApiKeyFilter extends OncePerRequestFilter {
+
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String requestApiKey = request.getHeader("X-API-KEY");
+
+        if (apiKey.equals(requestApiKey)) {
+            // continue request
+            filterChain.doFilter(request, response);
+        } else {
+            // block request
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Invalid or missing API Key");
+        }
+    }
+}
